b6f61856310d3218a48c0eb0e28ba65030bb8405
MaheswariKommuri <maheswarikommuri913@gmail.com>
2024-07-05 09:27:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UseEffect from './userEffect';
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+        <UseEffect />
+        </header>
+      </div>
+  );
+}
+
+export default App;
---
+++ first.js
@@ -0,0 +1,11 @@
+import logo from "react";
+function add()
+{
+    return(
+        <div>
+        <h1>HELLO WORLD</h1>
+        <h1>HELLO WORLD</h1>
+        </div>
+    );
+}
+
