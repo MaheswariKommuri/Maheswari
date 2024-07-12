7ec06eb6c97ffbd5754ffd59d667902f2a06bcc6
MaheswariKommuri <maheswarikommuri913@gmail.com>
2024-07-12 12:10:40 +0530

    Add files via upload

---
+++ QRCodeGenerator.js
@@ -0,0 +1,24 @@
+const express =require('express');
+const { toDataURL }=require('qrcode');
+const port =4000;
+const app=express();
+app.get('/pratap',(req, res) =>
+{
+    const url='https://github.com/login';
+    toDataURL(url,(err,qrcode)=>{
+        if(err)
+        {
+            res.status(500).send("internal server error");
+        }
+        else{
+            res.send(`
+                    <body>QR Code Generator
+                    <img src=" ${ qrcode} " alt="QR Code">
+                    </body>`
+                );
+        }
+    });
+});
+app.listen(port,()=>{
+    console.log(`Server is runnung on port ${port}`);
+});
